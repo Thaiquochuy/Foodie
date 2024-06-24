3b7be2ba5ae81e56cecbb54e35d0c5fb0f6ec2ed
thaiq <thaiq@DESKTOP-CEKF3RJ>
2024-06-24 16:44:10 +0700

    Add project files.

---
+++ Foodie/Admin/Product.aspx.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace Foodie.Admin
+{
+    public partial class Product : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Session["breadCrum"] = "Product";
+                if (Session["admin"] == null)
+                {
+                    Response.Redirect("../User/Login.aspx");
+                }
+                else
+                {
+                    getProducts();
+                }
+            }
+            lblMsg.Visible = false;
+        }
+
+        protected void btnAddOrUpdate_Click(object sender, EventArgs e)
+        {
+            string actionName = string.Empty, imagePath = string.Empty, fileExtenstion = string.Empty;
+            bool isValidToExecute = false;
+            int ProductId = Convert.ToInt32(hdnId.Value);
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("Product_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", ProductId == 0 ? "INSERT" : "UPDATE");
+            cmd.Parameters.AddWithValue("@ProductId", ProductId);
+            cmd.Parameters.AddWithValue("@Name", txtName.Text.Trim());
+            cmd.Parameters.AddWithValue("@Description", txtDescription.Text.Trim());
+            cmd.Parameters.AddWithValue("@Price", txtPrice.Text.Trim());
+            cmd.Parameters.AddWithValue("@Quantity", txtQuantity.Text.Trim());
+            cmd.Parameters.AddWithValue("@CategoryId", ddlCategories.SelectedValue);
+            cmd.Parameters.AddWithValue("@IsActive", cbIsActive.Checked);
+            if (fuProductImage.HasFile)
+            {
+                if (Utils.IsvalidExtension(fuProductImage.FileName))
+                {
+                    Guid obj = Guid.NewGuid();
+                    fileExtenstion = Path.GetExtension(fuProductImage.FileName);
+                    imagePath = "Images/Product/" + obj.ToString() + fileExtenstion;
+                    fuProductImage.PostedFile.SaveAs(Server.MapPath("~/Images/Product/") + obj.ToString() + fileExtenstion);
+                    cmd.Parameters.AddWithValue("@ImageUrl", imagePath);
+                    isValidToExecute = true;
+                }
+                else
+                {
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Please select .jpg, .jpeg or .png image";
+                    lblMsg.CssClass = "alert alert-danger";
+                    isValidToExecute = false;
+                }
+            }
+            else
+            {
+                isValidToExecute = true;
+            }
+            if (isValidToExecute)
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    actionName = ProductId == 0 ? "inserted" : "updated";
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Product" + actionName + "secessful!";
+                    lblMsg.CssClass = "alert alert-success";
+                    getProducts();
+                    Clear();
+                }
+                catch (Exception ex)
+                {
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Error - " + ex.Message;
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+
+        private void getProducts()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Connection.GetConnectionString()))
+                {
+                    using (SqlCommand cmd = new SqlCommand("Product_Crud", con))
+                    {
+                        cmd.Parameters.AddWithValue("@Action", "SELECT");
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                        {
+                            dt = new DataTable();
+                            sda.Fill(dt);
+                            rProduct.DataSource = dt;
+                            rProduct.DataBind();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi
+                lblMsg.Visible = true;
+                lblMsg.Text = "Error: " + ex.Message;
+                lblMsg.CssClass = "alert alert-danger";
+            }
+        }
+
+        private void Clear()
+        {
+            txtName.Text = string.Empty;
+            txtDescription.Text = string.Empty;
+            txtQuantity.Text = string.Empty;
+            txtPrice.Text = string.Empty;
+            ddlCategories.ClearSelection();
+            cbIsActive.Checked = false;
+            hdnId.Value = "0";
+            btnAddOrUpdate.Text = "Add";
+            imgProduct.ImageUrl = string.Empty;
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        protected void rProduct_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            lblMsg.Visible = false;
+            con = new SqlConnection(Connection.GetConnectionString());
+            Console.WriteLine("e.CommandName: " + e.CommandName);
+            if (e.CommandName == "edit")
+            {
+                cmd = new SqlCommand("Product_Crud", con);
+                cmd.Parameters.AddWithValue("@Action", "GETBYID");
+                cmd.Parameters.AddWithValue("@ProductId", e.CommandArgument);
+                cmd.CommandType = CommandType.StoredProcedure;
+                sda = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                sda.Fill(dt);
+                txtName.Text = dt.Rows[0]["Name"].ToString();
+                txtDescription.Text = dt.Rows[0]["Description"].ToString();
+                txtPrice.Text = dt.Rows[0]["Price"].ToString();
+                txtQuantity.Text = dt.Rows[0]["Quantity"].ToString();
+                ddlCategories.SelectedValue = dt.Rows[0]["CategoryId"].ToString();
+                cbIsActive.Checked = Convert.ToBoolean(dt.Rows[0]["IsActive"]);
+                imgProduct.ImageUrl = string.IsNullOrEmpty(dt.Rows[0]["ImageUrl"].ToString()) ?
+                        "../Images/No_image.png" : "../" + dt.Rows[0]["ImageUrl"].ToString();
+                imgProduct.Height = 200;
+                imgProduct.Width = 200;
+                hdnId.Value = dt.Rows[0]["ProductId"].ToString();
+                btnAddOrUpdate.Text = "Update";
+                LinkButton btn = e.Item.FindControl("lnkEdit") as LinkButton;
+                btn.CssClass = "badge badge-warning";
+            }
+            else if (e.CommandName == "delete")
+            {
+                cmd = new SqlCommand("Product_Crud", con);
+                cmd.Parameters.AddWithValue("@Action", "DELETE");
+                cmd.Parameters.AddWithValue("@ProductId", e.CommandArgument);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    con.Open();
+                    /*// Thực hiện xóa dữ liệu
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        // Xóa thành công, cập nhật giao diện
+                        lblMsg.Visible = true;
+                        lblMsg.Text = "Product deleted successfully";
+                        lblMsg.CssClass = "alert alert-success";
+                        getProducts();
+                    }
+                    else
+                    {
+                        // Xóa không thành công, xử lý lỗi cụ thể
+                        lblMsg.Visible = true;
+                        lblMsg.Text = "Error: Product not deleted.  Please check the ID and try again.";
+                        lblMsg.CssClass = "alert alert-danger";
+                    }*/
+                    cmd.ExecuteNonQuery();
+                    lblMsg.Text = "Product deleted successfully";
+                    lblMsg.CssClass = "alert alert-success";
+                    getProducts();
+                }
+                catch (Exception ex)
+                {
+                    // Xử lý lỗi chung
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Error: " + ex.Message;
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+
+        protected void rProduct_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                Label lblIsActive = e.Item.FindControl("lblIsActive") as Label;
+                Label lblQuantity = e.Item.FindControl("lblQuantity") as Label;
+                if (lblIsActive.Text == "True")
+                {
+                    lblIsActive.Text = "Active";
+                    lblIsActive.CssClass = "badge badge-success";
+                }
+                else
+                {
+                    lblIsActive.Text = "In-Active";
+                    lblIsActive.CssClass = "badge badge-danger";
+                }
+
+                if (Convert.ToInt32(lblQuantity.Text) <= 5)
+                {
+                    lblQuantity.CssClass = "badge badge-danger";
+                    lblQuantity.ToolTip = "Item about to be 'Out of stock'!";
+                }
+            }
+        }
+        protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+    }
+}
